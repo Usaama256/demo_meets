ad9bce0646454276179aa5fe9b8c72cd74f1758d
Usaama256 <usaamabinnasur@gmail.com>
2022-04-17 13:22:04 +0300

    _____

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,18 @@
+import styled from "styled-components";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Nav>
+        <h1>Meetups Demo</h1>
+        <ul>
+            <li>All Meetups</li>
+            <li>Favorites</li>
+            <li>Add Meetup</li>
+        </ul>
+    </Nav>
   );
 };
-
+const Nav = styled.div`
+    
+`;
 export default NavBar;
--- src/pages/AllMeets.jsx
+++ src/pages/AllMeets.jsx
@@ -1,8 +1,18 @@
+import styled from "styled-components";
+import MaterialNav from "../components/MaterialNav";
+import NavBar from "../components/NavBar";
 
 const AllMeets = () => {
   return (
-    <div className="home"></div>
+    <Container>
+      <MaterialNav />
+      <NavBar />
+    </Container>
   );
 };
 
+const Container = styled.div`
+    
+    
+`;
 export default AllMeets;
